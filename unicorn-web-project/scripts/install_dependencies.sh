27071bee7237a22f6ea6c7e3fe8722357d6debda
Manqoba <myoung023@student.wethinkcode.co.za>
2024-11-14 15:02:57 +0200

    new

--- unicorn-web-project/scripts/install_dependencies.sh
+++ unicorn-web-project/scripts/install_dependencies.sh
@@ -12,19 +12,3 @@ sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
     ProxyPassReverse / http://localhost:8080/unicorn-web-project/
 </VirtualHost>
 EOF
-
-#!/bin/bash
-sudo systemctl start tomcat.service
-sudo systemctl enable tomcat.service
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
-
-#!/bin/bash
-isExistApp="$(pgrep httpd)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
-fi
-isExistApp="$(pgrep tomcat)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
-fi
